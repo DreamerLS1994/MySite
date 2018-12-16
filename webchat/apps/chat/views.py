02b5bca1a829b8f871629dcf5abe6dedd8747aec
Jerry <454239755@qq.com>
2018-12-16 23:50:11 +0800

    误传文件夹webchat,删除文件夹

--- webchat/apps/chat/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def login_view(request):
-    #return HttpResponse("Hello World! From Jerry Coding")
-    return render(request, 'chat/index.html')
